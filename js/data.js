1a2245bcbb2702ed8bd6f61c9c770a9732c63d8e
sudhamhebbarbrown <sudham_hebbar@brown.edu>
2023-12-22 13:17:59 -0500

    [0000] - Intial commit

---
+++ js/data.js
@@ -0,0 +1,31 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Sudham Hebbar'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Sudham Hebbar'},
+    {name: 'Email', value: 'sudham_hebbar@brown.edu'},
+    {name: 'Phone', value: '+1 401-(255)-6524'},
+    {name: 'Country', value: 'United States of America'},
+    {name: 'City', value: 'Providence'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'ahsankhan26'}
+  ],
+  projects: [
+    {name: 'Book Blog', value: 'https://github.com/ahsankhan26/Book-Blog'},
+    {name: 'Bomberman Replica', value: 'https://github.com/ahsankhan26/Bomberman-Replica'},
+    {name: 'WhatsApp Chat Parser', value: 'https://github.com/ahsankhan26/Whatsapp-Chat-Parser'},
+    {name: 'Laser Data Transmission', value: 'https://github.com/ahsankhan26/Laser-Data-Transmission'},
+    {name: 'Color Guessing Game', value: 'https://github.com/ahsankhan26/Color-Guessing-Game'},
+    {name: 'GPA Calculator', value: 'https://github.com/ahsankhan26/gpa-calculator'},
+    {name: 'Hello World but unnecessarily complex', value: 'https://github.com/ahsankhan26/hello-world'}
+  ],
+  resume: 'Ahsan_Khan_CV.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://linkedin/in/ahsankhan26'},
+    {name: 'GitHub', value: 'https://github.com/ahsankhan26'},
+    {name: 'Instagram', value: 'https://instagram/ahsankhan26'},
+    {name: 'Website', value: 'https://ahsankhan.me'}
+  ]
+}
---
+++ js/script.js
@@ -0,0 +1,99 @@
+let input = document.querySelector("input");
+let terminalBody = document.querySelector("#terminalBody");
+
+let commandList = ['bio', "education", "skills", 'github', 'projects', 'clear', 'echo',  'name',  'resume', 'socials'];
+
+const functionCalls = () => {
+    checkWindowClick();
+    checkPressedEnter();
+}
+
+
+const checkWindowClick = () => {
+    terminalBody.addEventListener('click', () => input.focus());
+}
+
+const checkPressedEnter = () => {
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            execute(input.value);
+        }
+    });
+}
+
+const execute = (inputValue) => {
+    let temp = input.value;
+    input.remove();
+    terminalBody.innerHTML += temp;
+    checkCommand(temp);
+    addInput();
+}
+
+const executeCommand = (command) => {
+
+    terminalBody.innerHTML += '<br />'
+
+    for(let i=0; i<data[command].length; ++i){
+
+        terminalBody.innerHTML += `${data[command][i].name}: ` ;
+
+        if(data[command][i].value.includes('http')){
+            terminalBody.innerHTML += `<a href="${data[command][i].value}" target="_blank">${data[command][i].value}</a>` ;
+        } else{
+            terminalBody.innerHTML += `${data[command][i].value}` ;
+        }
+    }
+}
+
+const checkCommand = (inputCommand) => {
+
+    terminalBody.innerHTML += '<br>';
+    let command = inputCommand.split(" ")[0];
+
+    if(command){
+        if(command === 'clear'){ commandClear();}
+        else if(command === 'echo') {commandEcho(inputCommand);}
+        else if(command === 'github'){ commandGithub(command)}
+        else if(command === 'ls'){ commandHelp();}
+        else if(command === 'resume'){ commandResume();}
+        else if(command === 'sudo') { commandSudo();}
+        else if(commandList.includes(command)){ executeCommand(command);}
+        else{
+            terminalBody.innerHTML +=  inputCommand + ' is not recognized as a command, Try \"help\"';
+        }
+    }
+    terminalBody.innerHTML += '<br>';
+}
+
+function commandSudo() {
+    window.open('https://www.example.com', '_blank');
+}
+function addInput() {
+    terminalBody.innerHTML += '<span class="guest">guest</span>@<span class="localhost">localhost</span> :$ - <input type="text" autofocus />';
+    input = document.querySelector("input");
+    input.focus();
+    functionCalls();
+}
+
+const commandClear = () => { terminalBody.innerHTML = '' }
+
+function commandHelp() {
+    terminalBody.innerHTML += `<br />${commandList.join('<br />')}`;
+}
+
+function commandGithub() {
+    terminalBody.innerHTML += `<i class="fa fa-github"> <a href="https://github.com/${data.github[0].value}" target="_blank">${data.github[0].value}</a>`;
+}
+
+function commandResume() {
+    terminalBody.innerHTML += `<a href=assets/${data.resume}>Resume</a>`;
+}
+
+function commandEcho(inputCommand) {
+    for(let i=1; i<inputCommand.split(' ').length; i++){
+        terminalBody.innerHTML += inputCommand.split(' ')[i] + ' ';
+    }
+    terminalBody.innerHTML += '<br>';
+}
+
+functionCalls();
